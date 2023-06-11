3e8e267645e7cc1ee3383a954dda407bf034cf30
Edwin Ekpenyong <moogiemode@gmail.com>
2023-06-11 11:52:49 -0700

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,7 @@ export const metadata = {
   description: 'Open Tables is a restaurant reservation app. OpenTables Clone.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -36,7 +36,6 @@ const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
 
 export default async function RestaurantDetails({ params: { slug } }: { params: { slug: string } }) {
   const restaurant = await fetchRestaurantBySlug(slug);
-  console.log(restaurant);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
